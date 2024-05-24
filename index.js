7f668ba76e894e14b8dd692fe892f94ac51e21f7
Duong <s4031925@rmit.edu.vn>
2024-05-24 13:25:06 +0700

    add multer

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const User = require("./model/User");
 const bcrypt = require("bcrypt");
 const session = require("express-session");
 let app = express();
+const multer  = require('multer')
+const upload = multer({ dest: './public/uploads' })
 
 mongoose.connect("mongodb+srv://trieuduong:mithapnang12@colonthree.4y5dmo3.mongodb.net/?retryWrites=true&w=majority&appName=colonthree");
 
